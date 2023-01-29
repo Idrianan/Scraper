ea7850e718f7a97fdb8bccef6d68008f97c11dd8
Idrianan <47890348+Idrianan@users.noreply.github.com>
2023-01-29 21:11:06 +0300

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -21,4 +21,4 @@ def hello():
     return render_template('task.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader = False)
+    app.run(debug=False, host='0.0.0.0', port=5000, use_reloader = False)
